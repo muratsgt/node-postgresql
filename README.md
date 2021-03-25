e8fbf30016308da9ef8e183c7cf65a93022c3be5
murat <muratakca.mt@gmail.com>
2021-03-25 14:57:58 +0100

    database connection with sequelize

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 MURAT
-
 Node.js postgresql project
+
+
+.env file
+
+DB_USERNAME=
+DB_PASSWORD=
+DB_HOSTNAME=
+DB_PORT=
+DB_NAME=
--- app.js
+++ app.js
@@ -4,14 +4,39 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// for environment usage
+require("dotenv").config();
+
+// import routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var addUserRouter = require('./routes/adduser');
 
+// for database connection
+const { Sequelize } = require("sequelize");
+
+
+// initialize server
 var app = express();
+
+
+// connect to db
+// postgres://user:pass@example.com:5432/dbname
+const { DB_USERNAME, DB_PASSWORD, DB_HOSTNAME, DB_PORT, DB_NAME } = process.env;
+const sequelize = new Sequelize(
+  `postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOSTNAME}:${DB_PORT}/${DB_NAME}`
+);
+
+// test the connection
+sequelize.authenticate()
+  .then(() => console.log('Connection has been established successfully.'))
+  .catch((error) => console.error('Unable to connect to the database:', error));
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,14 +46,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/adduser', addUserRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
